4db7205b75bfddb801ee1232e1a38ca2e1539667
Arem58 <arem.9958@gmail.com>
2020-05-29 12:57:32 -0600

    configuraciones iniciales

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports ={
+    mode: 'development',
+    module: {
+        rules:[
+            {
+                test:/\.js$/,
+                use:{
+                    loader: 'babel-loader'
+            }   
+        }]
+    }
+}
